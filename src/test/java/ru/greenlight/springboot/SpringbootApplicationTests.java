1e9bf69af8bc6b056f7d3925a1a399a5b9e9d3fd
Vladislav Gutnik <lt38igreen@gmail.com>
2026-04-18 17:20:42 +0300

    Start job

---
+++ src/test/java/ru/greenlight/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.greenlight.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
